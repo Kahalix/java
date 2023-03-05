f3f304a1188f4104896f5694ebc7979eaa4dd2e7
deck <kahalix@gmail.com>
2023-03-05 14:44:11 +0100

    test

--- l1/l1.java
+++ l1/l1.java
@@ -2,5 +2,6 @@ public class l1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("1122");
+        System.out.println("67");
     }
 }
