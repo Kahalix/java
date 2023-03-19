62b73f2ff39c19ab39c907b430aa8cdb9e7f8fd8
deck <kahalix@gmail.com>
2023-03-19 16:00:15 +0100

    test

---
+++ src/z23.java
@@ -0,0 +1,9 @@
+public class z23 {
+    public static void main(String [] args)
+    {
+        double a = 5.23;
+        double b = 3.1;
+
+        System.out.println(a+b + " " + (a-b) + " " + a/b + " " + a*b);
+    }
+}
---
+++ src/z24.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class z24 {
+    public static void main(String [] args)
+    {
+        Scanner s=new Scanner(System.in);
+        double a = s.nextDouble();
+        double b = s.nextDouble();
+
+        System.out.println(a+b + " " + (a-b) + " " + a/b + " " + a*b);
+    }
+}
